2e5c766b5f57c69d8eba607c206bb40ea0b95007
marcSantolayaSanchez <146827090+marcSantolayaSanchez@users.noreply.github.com>
2024-10-28 16:34:45 +0100

    Ejercicio 6 y 7 del apartado E, el 6 esta comentado por que si no esto no FURULA!

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+## EJERCICIO 6
+## def on_forever():
+    ##    temperatura = input.temperature()
+    ##    led.plot_bar_graph(temperatura, 50)
+    ## basic.forever(on_forever)
 
+## EJERCICIO 7
+x = 2
+y = 2
+def on_forever():
+    global x,y
+    led.plot(x, y)
+    pause(50)
+    led.unplot(x, y)
+    accX = input.acceleration(Dimension.X)
+    accY = input.acceleration(Dimension.Y)
+    if (accX <= 50) and (x > 0):
+        x = x - 1
+    if (accX > 50) and (x < 4):
+        x = x + 1
+    if (accY <= 50) and (y > 0):
+        y = y - 1
+    if (accY > 50) and (y < 4):
+        y = y + 1    
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
-
+// # EJERCICIO 6
+// # def on_forever():
+// #    temperatura = input.temperature()
+// #    led.plot_bar_graph(temperatura, 50)
+// # basic.forever(on_forever)
+// # EJERCICIO 7
+let x = 2
+let y = 2
+basic.forever(function on_forever() {
+    
+    led.plot(x, y)
+    pause(50)
+    led.unplot(x, y)
+    let accX = input.acceleration(Dimension.X)
+    let accY = input.acceleration(Dimension.Y)
+    if (accX <= 50 && x > 0) {
+        x = x - 1
+    }
+    
+    if (accX > 50 && x < 4) {
+        x = x + 1
+    }
+    
+    if (accY <= 50 && y > 0) {
+        y = y - 1
+    }
+    
+    if (accY > 50 && y < 4) {
+        y = y + 1
+    }
+    
+})
